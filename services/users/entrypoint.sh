270f35e68f9ba631d8c6294376b3a08ff5e31877
Savio A <savio.abuga@andela.com>
2019-01-28 09:36:48 +0300

    update entrypoint file

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-echo "Waiting for postgres to start ..."
+echo "waiting for postgress..."
 while ! nc -z users-db 5432; do
     sleep 0.1
 done
-echo "Postgres started"
-
+echo "PostgreSQL started"
 python manage.py run -h 0.0.0.0
