a352e292645bee72afd80dc3a9c4192d5acb5128
Savio A <savio.abuga@andela.com>
2019-01-29 11:40:09 +0300

    use state to store users

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 
 class App extends Component {
+  state = { users: [] };
+
   componentDidMount() {
     this.getUsers();
   }
@@ -11,7 +13,7 @@ class App extends Component {
     axios
       .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
       .then(res => {
-        console.log(res);
+        this.setState({ users: res.data.data.users });
       })
       .catch(err => {
         console.log(err);
@@ -26,6 +28,13 @@ class App extends Component {
             <div className="column is-one-third">
               <br />
               <h1 className="title is-1">All Users</h1>
+              {this.state.users.map(user => {
+                return (
+                  <h4 key={user.id} className="box title is-4">
+                    {user.username}
+                  </h4>
+                );
+              })}
             </div>
           </div>
         </div>
