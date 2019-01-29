eb21ec714bd5531260727bc88a590e10328090fa
Savio A <savio.abuga@andela.com>
2019-01-29 15:04:25 +0300

    create a new component of User List

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import UserList from "./components/UserList";
 
 class App extends Component {
   state = { users: [] };
@@ -28,13 +29,7 @@ class App extends Component {
             <div className="column is-one-third">
               <br />
               <h1 className="title is-1">All Users</h1>
-              {this.state.users.map(user => {
-                return (
-                  <h4 key={user.id} className="box title is-4">
-                    {user.username}
-                  </h4>
-                );
-              })}
+              <UserList users={this.state.users} />
             </div>
           </div>
         </div>
