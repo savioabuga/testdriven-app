e86eead08294b418e11ad7be0318bd9bdbf5776c
Savio A <savio.abuga@andela.com>
2019-01-29 09:38:20 +0300

    add code coverage

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,7 +1,13 @@
 import unittest
+import coverage
 from flask.cli import FlaskGroup
 from project import create_app, db
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True, include="project/*", omit=["project/tests/*", "project/config.py"]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -31,6 +37,22 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()  # noqa
+
+
+@cli.command()
+def cov():
+    """runs tests"""
+    tests = unittest.TestLoader().discover("project/tests")
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print("Coverage Summary")
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 
 if __name__ == "__main__":
