833e5bcb7b2b7a383b95eb78698a0a2bfae380d5
Savio A <savio.abuga@andela.com>
2019-01-29 11:29:57 +0300

    update index.js to fetch users

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-const App = () => {
+class App extends Component {
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    axios
+      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
     return (
-        <section className="section">
-            <div className="cointainer">
-                <div className="columns">
-                    <div className="column is-one-third">
-                        <br/>
-                        <h1 className="title is-1">All Users
-                        </h1>
-                    </div>
-                </div>
+      <section className="section">
+        <div className="cointainer">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br />
+              <h1 className="title is-1">All Users</h1>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
