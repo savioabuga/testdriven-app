4b5d35137dd9a42855eef55baa4e2171df077af9
Savio A <savio.abuga@andela.com>
2019-01-30 09:37:30 +0300

    add posting of user data to users service

--- services/client/src/components/AddUser.js
+++ services/client/src/components/AddUser.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-const AddUser = () => {
+const AddUser = ({ username, email, addUser, handleChange }) => {
   return (
-    <form>
+    <form onSubmit={addUser}>
       <div className="field">
         <input
           name="username"
           type="text"
           className="input is-large"
           placeholder="Enter a username"
+          value={username}
+          onChange={handleChange}
           required
         />
       </div>
@@ -18,6 +20,8 @@ const AddUser = () => {
           className="input is-large"
           name="email"
           placeholder="Enter an email address"
+          value={email}
+          onChange={handleChange}
           required
         />
       </div>
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -5,10 +5,39 @@ import UserList from "./components/UserList";
 import AddUser from "./components/AddUser";
 
 class App extends Component {
-  state = { users: [] };
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.addUser = this.addUser.bind(this);
+  }
+
+  state = { users: [], username: "", email: "" };
 
   componentDidMount() {
     this.getUsers();
+  }
+
+  addUser(e) {
+    e.preventDefault();
+
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+
+    axios
+      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then(res => {
+        this.getUsers();
+        this.setState({ username: "", email: "" });
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleChange(e) {
+    const obj = {};
+    obj[e.target.name] = e.target.value;
+    this.setState(obj);
   }
 
   getUsers() {
@@ -32,7 +61,12 @@ class App extends Component {
               <h1 className="title is-1">All Users</h1>
               <hr />
               <br />
-              <AddUser />
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
               <br />
               <br />
               <UserList users={this.state.users} />
