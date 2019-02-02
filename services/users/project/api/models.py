723bc87e2cafe10419985f694ec53b6fcb51f784
Savio A <savio.abuga@andela.com>
2019-02-02 10:12:15 +0300

    add password as table field

--- services/users/project/api/models.py
+++ services/users/project/api/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.sql import func
-from project import db
+from project import db, bcrypt
 
 
 class User(db.Model):
@@ -9,11 +9,12 @@ class User(db.Model):
     email = db.Column(db.String(128), unique=True, nullable=False)
     active = db.Column(db.Boolean, default=True, nullable=False)
     created = db.Column(db.DateTime, default=func.now(), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
-        self.password = password
+        self.password = bcrypt.generate_password_hash(password).decode()
 
     def to_json(self):
         return {
