eadc7ad05b681ab87bedcc629529c402dcdb4da8
Savio A <savio.abuga@andela.com>
2019-02-02 09:36:25 +0300

    create a util method add user

--- services/users/project/tests/test_users_model.py
+++ services/users/project/tests/test_users_model.py
@@ -2,40 +2,33 @@ import unittest
 from sqlalchemy.exc import IntegrityError
 
 from project.api.models import User
+from project.tests.utils import add_user
 from project import db
 from project.tests.base import BaseTestCase
 
 
 class TestUserModel(BaseTestCase):
     def test_add_user(self):
-        user = User(username="justatest", email="test@test.com")
-        db.session.add(user)
-        db.session.commit()
+        user = add_user(username="justatest", email="test@test.com")
         self.assertTrue(user.id)
         self.assertEqual(user.username, "justatest")
         self.assertEqual(user.email, "test@test.com")
         self.assertTrue(user.active)
 
     def test_adding_duplicate_username(self):
-        user = User(username="justatest", email="test@test.com")
-        db.session.add(user)
-        db.session.commit()
+        add_user(username="justatest", email="test@test.com")
         duplicate_user = User(username="justatest", email="test@test2.com")
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_adding_duplicate_email(self):
-        user = User(username="justatest", email="test@test.com")
-        db.session.add(user)
-        db.session.commit()
+        add_user(username="justatest", email="test@test.com")
         duplicate_user = User(username="justatest1", email="test@test.com")
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_to_jsons(self):
-        user = User(username="justatest", email="test@test.com")
-        db.session.add(user)
-        db.session.commit()
+        user = add_user(username="justatest", email="test@test.com")
         self.assertTrue(isinstance(user.to_json(), dict))
 
 
