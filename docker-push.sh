cfa2ff83163ce9e511652ddfe688d88db56d695b
Savio A <savio.abuga@andela.com>
2019-02-20 16:54:43 +0300

    update staging with load balancer dns

--- docker-push.sh
+++ docker-push.sh
@@ -5,7 +5,7 @@ then
 
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
-    export REACT_APP_USERS_SERVICE_URL="testdriven-staging-alb-806588837.us-west-1.elb.amazonaws.com"
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-806588837.us-west-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
   fi
