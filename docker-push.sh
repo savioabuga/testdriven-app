b6aaa4c6c44d624595bd4044c0ff63d36cda5958
Savio A <savio.abuga@andela.com>
2019-04-05 08:57:17 +0300

    no need to install aws

--- docker-push.sh
+++ docker-push.sh
@@ -12,10 +12,10 @@ then
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
      [ "$TRAVIS_BRANCH" == "production" ]
   then
-    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-    unzip awscli-bundle.zip
-    ./awscli-bundle/install -b ~/bin/aws
-    export PATH=~/bin:$PATH
+    # curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+    # unzip awscli-bundle.zip
+    # ./awscli-bundle/install -b ~/bin/aws
+    # export PATH=~/bin:$PATH
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
     eval $(aws ecr get-login --region us-west-1 --no-include-email)
     export TAG=$TRAVIS_BRANCH
