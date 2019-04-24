520f014ad5c51c8828f401472c1ccd1d0e7582f9
Savio A <savio.abuga@andela.com>
2019-04-24 10:04:13 +0300

    remove aws login

--- docker-push-jenkins.sh
+++ docker-push-jenkins.sh
@@ -21,7 +21,7 @@ then
     # ./awscli-bundle/install -b ~/bin/aws
     # export PATH=~/bin:$PATH
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
-    eval $(aws ecr get-login --region us-west-1 --no-include-email)
+    # eval $(aws ecr get-login --region us-west-1 --no-include-email)
     export TAG=$BRANCH
     export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com
   fi
